752616bf6b78b26a025776d3d2f21eb94b1e6818
Anastasiya Komkova <nastia.romanceva@mail.ru>
2021-11-01 02:13:17 +0300

    prodject

--- js/main.js
+++ js/main.js
@@ -1,12 +1,71 @@
 $(function(){
-    $('.catmenu').click(function(){
-        $(this).toggleClass('open');
-    })
+
+
+
+
+
+  if ($('.table').length) {
+        writeTable();
+        $(document).on('click', '.quantity button', function(){
+            let delta = 1;
+            if (this.innerHTML == '-') {
+                delta = -1;
+            }
+            let id = +$(this).parents('tr').find('.id').prop('id').slice(6);
+            for (item of tovardata) {
+                if (item.id == id) {
+                    item.qty += delta;
+                    if (item.qty <= 0) removeTovar(id);
+                   break;
+                }
+            }
+			 writeTable();
+        })
+        $(document).on('click', '.delete button', function(){
+            let id = +$(this).parents('tr').find('.id').prop('id').slice(6);
+            if (removeTovar(id)) writeTable();
+        })
+		$('#date').focus(makeCalendar);
+        $('.form button').click(function(){
+            makeOrder();
+        })
+    }
     
-    setInterval(retimer, 500);
-})
-
-
+    if ($('.gallery').length) {
+        galstep = $('.smallimage').width(); // померили ширину блока с мелкой картинкой
+        galgap = parseInt($('.gallery_rail').css('gap')); // померили промежуток между картинками
+        /* подключаем кнопки */
+        $('.g_left').click(function(){
+            galSlide('left'); // если двигать блок некуда, кнопка не видна - и нажать на нее не получится. если по дизайну неактивная кнопка должна быть видна, тут лучше сделать проверку класса.
+        });
+        $('.g_right').click(function(){
+            galSlide('right');
+        });
+        $('.smallimage img').click(function(){
+            $('.bigimage img').prop('src', $(this).prop('src').split('min').join('big'));
+        });
+        $('.bigimage img').click(function(){
+            lightbox(this);
+        });
+    }
+    
+    if ($('.catalog').length) {
+        $('.accordeon, .accordeon .level2').hide();
+        $('.mom').click(function(){
+            $(this).toggleClass('open');
+            $('.accordeon').toggle('slow');
+        })
+        $('.accordeon .level1 > .menupoint').click(function(){
+            if ($(this).parent().find('div').length && !$(this).parent().hasClass('open')) {
+                $('.level1.open').removeClass('open').find('.level2').hide('slow');
+                $(this).parent().addClass('open').find('.level2').show('slow');
+                return false;
+            }
+        });
+    }
+    
+    
+  
 
 
    if ($('.retaimer').length) {
@@ -15,87 +74,29 @@ $(function(){
     }
 	
 	
-	 if ($('.slider_block').length) {
+	 if ($('slider_block').length) {
         setInterval(function(){
             sliderRun('toleft');
         }, 4000);
-        $('.slider .to_left').click(function(){
+        $('.slider .fa-angle-left').click(function(){
             sliderRun('toleft');
         });
-        $('.slider .to_right').click(function(){
+        $('.slider .fa-angle-right').click(function(){
             sliderRun('toright');
         });
     }
-let slideFlag = false;
+	
+	
+	
+let menuElem = document.querySelectorAll('.mom');
+let titleElem = document.querySelectorAll('.title');
+
+    titleElem.onclick = function() {
+      menuElem.classList.toggle('open');
+    };
+	
+
+})
 
 
-/*function slideRight() {
-	let hlp = $('.slider_block').index($('.slider_block.prev')) - 1;
-	if (hlp < 0) hlp += $('.slider_block').length;
-	$('.slider_block.next').removeClass('next');
-	$('.slider_block.curr').removeClass('curr').addClass('next');
-	$('.slider_block.prev').removeClass('prev').addClass('curr');
-	$('.slider_block').eq(hlp).addClass('prev');
-}
-*/
-function sliderRun(direction) {
-    if (slideFlag) return;
-    slideFlag = true;
-    let hlp = $('.slider_block').index($('.slider_block.curr'));
-    let width = $('.slider_block.curr').width();
-    let next;
-    if (direction == 'toleft') {
-        next = hlp + 1;
-        if (next > $('.slider_block').length - 1) next -= $('.slider_block').length;
-        $('.slider_block').eq(next).css('left', width + 'px').addClass('curr');
-        next = '-=' + width;
-    } else if (direction == 'toright') {
-        next = hlp - 1;
-        if (next < 0) next += $('.slider_block').length;
-        $('.slider_block').eq(next).css('left', -width + 'px').addClass('curr');
-        next = '+=' + width;
-    } else {
-        console.error('invalid direction');
-        slideFlag = false;
-        return;
-    }
-    $('.slider_block.curr').animate({left: next}, 2000, function() {
-        $('.slider_block').eq(hlp).removeClass('curr').prop('style','');
-        slideFlag = false;
-    });
-}
 
-function retimer() {
-    let limit = new Date($('.retaimer').data('fordate'));
-    let now = new Date();
-    let delta = Math.floor((limit.getTime() - now.getTime()) / 1000);
-    if (delta < 0) delta = 0;
-	let sec = delta % 60
-    $('.retaimer .num')[3].innerHTML = `${addChar(sec)}<span class="subnum">${multiple(sec, ['секунда', 'секунды', 'секунд'])}</span>`;
-    delta = Math.floor(delta / 60);
-    let minute = delta % 60;
-    $('.retaimer .num')[2].innerHTML = `${addChar(minute)}<span class="subnum">${multiple(minute, ['минута', 'минуты', 'минут'])}</span>`;
-    delta = Math.floor(delta / 60);
-    let hour = delta % 24;
-    $('.retaimer .num')[1].innerHTML = `${addChar(hour)}<span class="subnum">${multiple(hour, ['час', 'часа', 'часов'])}</span>`;
-    delta = Math.floor(delta / 24);
-    $('.retaimer .num')[0].innerHTML = `${delta}<span class="subnum">${multiple(delta, ['день', 'дня', 'дней'])}</span>`;
-}
-function addChar(c) {
-    c += '';
-    if (c.length < 2) {
-        c = '0' + c;
-    }
-    return c;
-}
-function multiple(num, words) {
-    num = num % 100;
-    if (Math.floor(num / 10) != 1) {
-        if (num % 10 == 1) {
-            return words[0];
-        } else if ((num % 10 > 1) && (num % 10 < 5)) {
-            return words[1];
-        }
-    }
-    return words[2];
-}
